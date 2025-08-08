0d4b9e6d7054c864202df2312fc0bffd2f15a876
DoctorSulla <matthew.halliday@gmail.com>
2025-08-08 08:10:31 +0100

    Latest

--- config.fish
+++ config.fish
@@ -1,3 +1,12 @@
+set --erase LD_PRELOAD
+
+fish_add_path /home/matt/.cargo/bin/
+fish_add_path /home/matt/.npm/_npx/38c708f8d73fe4c9/node_modules/.bin/
+
+source ~/.config/fish/api_keys.fish
+
+export MOZ_ENABLE_WAYLAND=1
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -29,9 +38,12 @@ function novpn
 end
 
 function web
-	flatpak run io.github.zen_browser.zen &
+	flatpak run app.zen_browser.zen &
 end
 
 function ter
 	alacritty &
 end
+
+# opencode
+fish_add_path /home/matt/.opencode/bin
